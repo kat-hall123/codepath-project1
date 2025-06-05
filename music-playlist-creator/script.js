c8815f4eaf2be222ea286a85ba18fd0817eb7310
Katherine Hall <khall206@meta.com>
2025-06-04 22:37:48 -0700

    displayed playlist cards dynamically

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,58 @@
+function loadPlaylists() {
+    fetch('data/data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Successfully loaded playlist cards", data);
 
+        const playlistList = document.getElementById('playlist-list');
+        playlistList.innerHTML = '';
+
+        if(!data || data.length == 0) {
+            playlistList.innerHTML = '<p>No playlists added.</p>';
+            return;
+        }
+
+        data.forEach(playlist => {
+            const playlistCard = createPlaylistCard(playlist);
+            playlistList.appendChild(playlistCard);
+        })
+    })
+    .catch(error => {
+        console.error('Error loading reviews:', error);
+    })
+}
+
+function createPlaylistCard(playlist) {
+    const cardSec = document.createElement('section');
+    cardSec.className = 'card';
+
+    const imgElem = document.createElement('img');
+    imgElem.src = playlist.playlist_art;
+    imgElem.width = 200;
+    imgElem.alt = "Picture of playlist";
+    
+    const titleElem = document.createElement('h2');
+    titleElem.textContent = playlist.playlist_name;
+
+    const authorElem = document.createElement('h3');
+    authorElem.textContent = playlist.playlist_author;
+
+    const likeCount = document.createElement('p');
+    likeCount.textContent = '5';
+
+    cardSec.appendChild(imgElem);
+    cardSec.appendChild(titleElem);
+    cardSec.appendChild(authorElem);
+    cardSec.appendChild(likeCount);
+
+    return cardSec;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadPlaylists();
+});
